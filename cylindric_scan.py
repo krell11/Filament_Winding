6b773adaa19d88be0011d77e076e80b2a8cebd2c
Kirill Bobin <11bobin.kiryusha@gmail.com>
2023-06-28 19:55:00 +0300

    global code remastering + add multiple functions

--- cylindric_scan.py
+++ cylindric_scan.py
@@ -7,11 +7,13 @@ class Wrapp:
     def __init__(self, func, radius, n, phi):
         self.func = func
         self.radius = 2 * np.pi * radius
-        self.x = np.linspace(0, self.radius, 100)
+        self.x = np.linspace(0, self.radius, 10)
         self.n = n
         self.phi = phi * (np.pi/180)
         self.interceptions = np.empty((0, 2))
         self.linear_points = np.empty((0, 2))
+        self.point_for_projection = np.empty((0, 3))
+        self.theta = []
 
     def create_spirals(self, start_x, height):
         tmp = np.empty((0, 2))
@@ -19,11 +21,12 @@ class Wrapp:
             if start_x == 0:
                 f_val = math.tan(self.phi) * j
             else:
-                f_val = math.tan(self.phi) * j + abs(self.radius / start_x - self.radius)
+                f_val = math.tan(self.phi) * j + (self.radius / start_x - self.radius)
             f_val += height
             if f_val <= self.func:
                 new_point = np.array([j, f_val])
                 self.linear_points = np.vstack((self.linear_points, new_point))
+                self.theta.append(j / self.radius)
                 if j == self.radius:
                     tmp = self.create_spirals(0, f_val)
             else:
@@ -34,12 +37,10 @@ class Wrapp:
             return np.vstack((self.linear_points, tmp))
 
     def n_spirals(self):
-        mass_point = np.empty((0, 2))
+        mass_point = self.linear_points
         for i in range(self.n):
             startpoint = self.radius - self.radius/(i+1)
-            print(startpoint)
             tmp_i = self.create_spirals(startpoint, 0)
-            print(tmp_i)
             mass_point = np.vstack((mass_point, tmp_i))
         return mass_point
 
@@ -55,16 +56,32 @@ class Wrapp:
         plt.scatter(x, y)
         plt.show()
 
-    def back_wrapp(self):
-        points = self.n_spirals()
-        fin_points = np.empty((0,3))
-        #for i in points:
-# доделать приближение с малой хордой dx и дугой окружности, по идее там приближение будет достаточно
+    def plotter_cylindric_shape(self, points):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.set_xlim(left=-a.radius, right=a.radius)
+        ax.set_ylim(bottom=0, top=a.func)
+        x = points[:, 0]
+        y = points[:, 1]
+        z = points[:, 2]
+        ax.scatter(x, y, z)
+        plt.show()
+
+    def project_points_to_cylinder_side(self):
+        z = self.create_spirals(0, 0)
+        for i in range(len(self.linear_points)):
+            theta_value = self.theta[i]
+            x = self.radius*np.cos(theta_value)/(2*np.pi)
+            y = self.radius*math.sin(theta_value)/(2*np.pi)
+            z_i = z[i, 1]
+            tmp = np.array([x, y, z_i])
+            self.point_for_projection = np.vstack((self.point_for_projection, tmp))
+        return self.point_for_projection
 
 
-
-a = Wrapp(10, 3, 2, 20)
+a = Wrapp(10, 3, 1, 2)
 print(a)
-b = a.n_spirals()
-c = a.find_interceptions(b)
-d = a.plotter(b)
+c = a.project_points_to_cylinder_side()
+d = a.n_spirals()
+a.plotter_cylindric_shape(c)
+a.plotter(d)
---
+++ new_implementation.py
@@ -0,0 +1,90 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+np.set_printoptions(suppress=True)
+
+
+class Wrapp:
+    def __init__(self, height, r, phi, n, clearance):
+        self.height = height
+        self.phi = phi
+        self.clearance = clearance
+        self.r = r
+        self.r_pi = r * 2 * np.pi
+        self.x = np.linspace(0, self.r * 2 * np.pi, self.clearance)
+        self.points_on_plane = np.empty((0, 2))
+        self.theta = np.linspace(0, 2*np.pi, self.clearance)
+        self.n = n
+
+    def h_calc(self, phi):
+        h = self.r_pi * math.sin(phi)
+        return h
+
+    def period_calc(self):
+        return math.ceil(self.height / self.h_calc(self.phi))
+
+    def create_spirals(self, coordinates, step):
+        points = np.empty((0, 3))
+        total_height = 0
+        angle = 0
+        while total_height <= self.height:
+            x = round(self.r * math.cos(angle + coordinates), 3)
+            y = round(self.r * math.sin(angle + coordinates), 3)
+            z = abs(round(angle * self.h_calc(self.phi), 3))
+            point = np.array([[x, y, z]])
+            points = np.vstack((points, point))
+            total_height = z
+            angle += step
+        return points
+
+    def plotter_cylindric_shape(self, points):
+        x = points[:, 0]
+        y = points[:, 1]
+        z = points[:, 2]
+        min_height = np.min(z)
+        min_height_indices = np.where(z == min_height)[0]
+        colors = ['blue'] * len(x)
+        for index in min_height_indices:
+            colors[index] = 'red'
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(x, y, z, c=colors)
+        plt.show()
+
+    def calculate_angles(self, n):
+        angles = []
+        for i in range(n):
+            angles.append(abs(self.r_pi/n*i - self.r_pi))
+        return angles
+
+    def calculate_start_coordinates(self, angles):
+        x = []
+        y = []
+        for angle in angles:
+            x.append(self.r * np.cos(angle))
+            y.append(self.r * np.sin(angle))
+        return np.column_stack((x, y))
+
+    def create_multiple_spirals(self):
+        spirals = np.empty((0, 3))
+        spirals1 = np.empty((0, 3))
+        start_points = self.calculate_angles(self.n)
+        # start_points = self.calculate_start_coordinates(self.calculate_angles(self.n)) не работает
+        for i in range(self.n):
+            spiral = self.create_spirals(start_points[i], 0.02)
+            spirals = np.vstack((spirals, spiral))
+            spiral1 = self.create_spirals(start_points[i], -0.02)
+            spirals1 = np.vstack((spirals1, spiral1))
+        combined_spirals = np.vstack((spirals, spirals1))
+        return combined_spirals
+
+    def find_interceptions(self, all_points, tolerance):
+
+        return
+
+a = Wrapp(height=100, r=2, phi=2, n=9, clearance=1000)
+# есть недочет, нормально работает только  нечетными n
+b = a.create_multiple_spirals()
+c = a.plotter_cylindric_shape(b)
+duplicate_points = a.find_interceptions(b, 0.05)
+c = a.plotter_cylindric_shape(duplicate_points)
